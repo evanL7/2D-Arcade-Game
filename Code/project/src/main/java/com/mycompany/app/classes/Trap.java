b2086149e752605b850847292113c5e144f5113b
ktc15 <tim_chou@sfu.ca>
2024-02-29 10:51:03 -0800

    Created methods and attributes for Trap

--- Code/project/src/main/java/com/mycompany/app/classes/Trap.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Trap.java
@@ -1,5 +1,13 @@
 package com.mycompany.app.classes;
 
-public class Trap extends StaticEntity {
+public class Trap extends StaticEntity 
+{
+
+    private float damage;
+
+    public float getDamage()
+    {
+        return damage;
+    }
 
 }
