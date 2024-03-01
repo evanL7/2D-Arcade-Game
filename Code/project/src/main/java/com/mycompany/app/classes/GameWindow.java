a26d953ddd1825eed37696192b78e8edb69a94c0
Evan <eevanlaw@gmail.com>
2024-02-29 20:39:21 -0800

    added GameWindow constructor that uses JFrame to create a window to display our game

--- Code/project/src/main/java/com/mycompany/app/classes/GameWindow.java
+++ Code/project/src/main/java/com/mycompany/app/classes/GameWindow.java
@@ -1,5 +1,14 @@
 package com.mycompany.app.classes;
 
+import javax.swing.JFrame;
+
 public class GameWindow {
-    
+    private JFrame jframe;
+
+    public GameWindow() {
+        jframe = new JFrame();
+        jframe.setSize(400, 400);
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jframe.setVisible(true);
+    }
 }
