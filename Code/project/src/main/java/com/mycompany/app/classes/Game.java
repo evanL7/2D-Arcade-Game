b15f2ae6fc77e3b23e461fd53e9517079e7d8ca0
Evan <eevanlaw@gmail.com>
2024-03-02 10:37:48 -0800

    updated Game class to track state of keyboard presses

--- Code/project/src/main/java/com/mycompany/app/classes/Game.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Game.java
@@ -1,11 +1,13 @@
 package com.mycompany.app.classes;
 
 public class Game {
+
     private GameWindow gameWindow;
     private GamePanel gamePanel;
 
     public Game() {
         gamePanel = new GamePanel();
         gameWindow = new GameWindow(gamePanel);
+        gamePanel.requestFocus();
     }
 }
