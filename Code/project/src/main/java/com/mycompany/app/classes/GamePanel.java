deb38da3374163a5273ae5418169f9b84eb21194
Evan <eevanlaw@gmail.com>
2024-03-01 17:56:39 -0800

    updated screen settings

--- Code/project/src/main/java/com/mycompany/app/classes/GamePanel.java
+++ Code/project/src/main/java/com/mycompany/app/classes/GamePanel.java
@@ -1,14 +1,37 @@
 package com.mycompany.app.classes;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
-    public GamePanel() {
 
+    // Screen settings
+    final int originalTileSize = 16;
+    final int scale = 3;
+    final int tileSize = originalTileSize * scale;
+
+    // Maze size
+    final int maxScreenCol = 16;
+    final int maxScreenRow = 12;
+    
+    // Scaled screen size
+    final int screenWidth = tileSize * maxScreenCol;
+    final int screenHeight = tileSize * maxScreenRow;
+
+    public GamePanel() {
+        this.setBackground(Color.BLACK);
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
+    }
+
+    public int getScreenWidth() {
+        return screenWidth;
+    }
+
+    public int getScreenHeight() {
+        return screenHeight;
     }
 }
