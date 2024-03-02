f7e3c310e6f58d0aa807cd4b162b77d38f9114de
Evan <eevanlaw@gmail.com>
2024-03-02 10:28:58 -0800

    added code to recognize key presses for player movement

---
+++ Code/project/src/main/java/com/mycompany/app/classes/KeyboardInputs.java
@@ -0,0 +1,40 @@
+package com.mycompany.app.classes;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyboardInputs implements KeyListener{
+
+    private GamePanel gamePanel;
+
+    public KeyboardInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_W:
+                System.out.println("pressed W");
+                break;
+            case KeyEvent.VK_A:
+                System.out.println("pressed A");
+                break;
+            case KeyEvent.VK_S:
+                System.out.println("pressed S");
+                break;
+            case KeyEvent.VK_D:
+                System.out.println("pressed D");
+                break;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+    
+}
