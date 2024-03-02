6e33ba8410fb930c0d2162ad8880267a2bdccecc
cas32 <claire_shou@sfu.ca>
2024-03-02 13:13:34 -0800

    adjusted the attributes and methods in Enemy class

--- Code/project/src/main/java/com/mycompany/app/classes/Enemy.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Enemy.java
@@ -3,15 +3,16 @@ package com.mycompany.app.classes;
 import java.util.Vector;
 
 public class Enemy extends MoveableEntity {
+    // ATTRIBUTES
+    private Vector<Position> pathToPlayer;
+
     // CONSTRUCTOR
     public Enemy() {
         // need to determine the players start position and specific sprite
         speed = 1;
     }
 
-    // METHODS
-    private Vector<Position> pathToPlayer;
-
+    // METHOD
     public void updateShortestPath() {
 
     }
