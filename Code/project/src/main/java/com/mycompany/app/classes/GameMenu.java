a39e14df61bfbaedc999ce8bba9c599136b9f7c1
mza128 <mza128@sfu.ca>
2024-03-02 14:54:59 -0800

    added functions to BoardData

--- Code/project/src/main/java/com/mycompany/app/classes/GameMenu.java
+++ Code/project/src/main/java/com/mycompany/app/classes/GameMenu.java
@@ -1,5 +1,24 @@
 package com.mycompany.app.classes;
 
 public class GameMenu {
+    
+    // Function to start the game
+    public void start() {
+        
+    }
+    
 
+    public void customize() {
+        
+    }
+    
+   
+    public void settings() {
+       
+    }
+    
+   
+    public void exit() {
+       
+    }
 }
