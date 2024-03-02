1e3d2ec7915fd601423bdc0e9c7bda1c6b3b4243
ktc15 <tim_chou@sfu.ca>
2024-03-02 11:50:02 -0800

    Removed difficulty from Reward.java as it is unnecessary

--- Code/project/src/main/java/com/mycompany/app/classes/Reward.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Reward.java
@@ -8,8 +8,6 @@ public class Reward extends StaticEntity
     private int rewardsToCollect;
     public RewardType rewardType;
 
-    //I added another attribute for difficulty as that will determine how many rewards a player should collect
-    public Difficulty difficulty;
 
     public double getRewardAmount()
     {
@@ -29,21 +27,7 @@ public class Reward extends StaticEntity
     //I changed the return type for this method to void since I believe it shouldn't return anything
     public void setRewardsToCollect()
     {
-        // the difficulty determines the number of rewards to collect
-        switch (difficulty)
-        {
-            case Easy:
-                this.rewardsToCollect = 3;
-                break;
-            
-            case Medium:
-                this.rewardsToCollect = 5;
-                break;
-            
-            case Hard:
-                this.rewardsToCollect = 7;
-                break;
-        }
+
     }
 
 }
