a47ef555be7748da9495cde72076b46b24ce72a2
Evan <eevanlaw@gmail.com>
2024-03-01 17:34:32 -0800

    added paintComponent method

--- Code/project/src/main/java/com/mycompany/app/classes/GamePanel.java
+++ Code/project/src/main/java/com/mycompany/app/classes/GamePanel.java
@@ -1,9 +1,14 @@
 package com.mycompany.app.classes;
 
+import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel {
     public GamePanel() {
 
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
     }
 }
