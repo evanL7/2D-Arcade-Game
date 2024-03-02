e6c2489b927b7ef23d7b068973ddafad9597a387
cas32 <claire_shou@sfu.ca>
2024-03-02 14:51:37 -0800

    added Main class into classes folder

---
+++ Code/project/src/main/java/com/mycompany/app/classes/Main.java
@@ -0,0 +1,11 @@
+package com.mycompany.app.classes;
+
+import com.mycompany.app.classes.Display.Game;
+
+public class Main {
+    public static void main(String[] args) {
+        // Displays game window
+        new Game();
+    }
+
+}
