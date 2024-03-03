d6ff1f6f3f5b513e800546b1caf30bf5c195213a
ktc15 <tim_chou@sfu.ca>
2024-03-03 13:28:45 -0800

    Removed a parameter from the constructor and added 1 more comment

--- Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/Trap.java
+++ Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/Trap.java
@@ -1,5 +1,6 @@
 package com.mycompany.app.classes.StaticEntity;
 import java.awt.*;
+import com.mycompany.app.classes.Helpers.Position;
 
 public class Trap extends StaticEntity 
 {
@@ -7,12 +8,14 @@ public class Trap extends StaticEntity
     private float damage;
 
     // constructor
-    public Trap(int x, int y, boolean objectDespawns, int despawnTimer, Image sprite, float damage)
+    // doesn't need despawnTimer as it would only despawn if collided with
+    public Trap(Position position, boolean objectDespawns, Image sprite, float damage)
     {
-        super(x, y, objectDespawns, despawnTimer, sprite);
+        super(position, objectDespawns, sprite);
         this.damage = damage;
     }
 
+    // Methods
     public float getDamage() 
     {
         return damage;
