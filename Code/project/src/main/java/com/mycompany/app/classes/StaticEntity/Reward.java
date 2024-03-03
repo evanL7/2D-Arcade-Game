bb90ea8c2db8e88454b124ebe3aeae915db920c3
ktc15 <tim_chou@sfu.ca>
2024-03-03 13:49:13 -0800

    Added two constructors and added parameters to setter methods

--- Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/Reward.java
+++ Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/Reward.java
@@ -1,6 +1,8 @@
 package com.mycompany.app.classes.StaticEntity;
 
 import com.mycompany.app.classes.Helpers.RewardType;
+import java.awt.*;
+import com.mycompany.app.classes.Helpers.Position;
 
 public class Reward extends StaticEntity {
 
@@ -8,22 +10,44 @@ public class Reward extends StaticEntity {
     private int rewardsToCollect;
     public RewardType rewardType;
 
-    public float getRewardAmount() {
+    // constructor for BonusRewards
+    public Reward(Position position, boolean objectDespawns, int despawnTimer, Image sprite, float rewardAmount, int rewardsToCollect)
+    {
+        super(position, objectDespawns, despawnTimer, sprite);
+        this.rewardAmount = rewardAmount;
+        this.rewardsToCollect = rewardsToCollect;
+        
+        // since the only reward that also relies on despawnTimer to despawn are Bonus Rewards
+        rewardType = RewardType.BonusReward;
+    }
+
+    // constructor for Regular Rewards
+    public Reward(Position position, boolean objectDespawns, Image sprite, float rewardAmount, int rewardsToCollect)
+    {
+        super(position, objectDespawns, sprite);
+        this.rewardAmount = rewardAmount;
+        this.rewardsToCollect = rewardsToCollect;
+        rewardType = RewardType.RegularReward;
+    }
+
+    public float getRewardAmount() 
+    {
         return rewardAmount;
     }
 
-    public void setRewardAmount() {
-        this.rewardAmount = 0.5f;
+    public void setRewardAmount(float rewardAmount) 
+    {
+        this.rewardAmount = rewardAmount;
     }
 
-    public int getRewardsToCollect() {
+    public int getRewardsToCollect() 
+    {
         return rewardsToCollect;
     }
 
-    // I changed the return type for this method to void since I believe it
-    // shouldn't return anything
-    public void setRewardsToCollect() {
-
+    public void setRewardsToCollect(int rewardsToCollect) 
+    {
+        this.rewardsToCollect = rewardsToCollect;
     }
 
 }
