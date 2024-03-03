d53761e51696eb0e4162c3cc46f31f5b80627bd8
ktc15 <tim_chou@sfu.ca>
2024-03-03 10:24:31 -0800

    Added a constructor for StaticEntity and commented out some lines that might be removed later

--- Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/StaticEntity.java
+++ Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/StaticEntity.java
@@ -1,15 +1,30 @@
 package com.mycompany.app.classes.StaticEntity;
 
 import java.awt.*;
-import java.util.Timer;
+
+
+//import java.util.Timer;
 
 abstract class StaticEntity {
 
     protected int x;
     protected int y;
     protected boolean objectDespawns;
-    protected Timer despawnTimer;
+    //protected Timer despawnTimer;
+    protected int despawnTimer;
     public Image sprite;
+
+    // Constructor
+
+    public StaticEntity(int x, int y, boolean objectDespawns, int despawnTimer, Image sprite)
+    {
+        this.x = x;
+        this.y = y;
+        this.objectDespawns = objectDespawns;
+        this.despawnTimer = despawnTimer;
+        this.sprite = sprite;
+    }
+
 
     public void onCollide() {
 
