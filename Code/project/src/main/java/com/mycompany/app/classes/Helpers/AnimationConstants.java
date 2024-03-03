cc81d0fb941c96661e77fc3cac04d1d437b0c798
cas32 <cas32@sfu.ca>
2024-03-02 23:12:46 -0800

    created a new class AnimationConstants to keep static values of sprite index values.

---
+++ Code/project/src/main/java/com/mycompany/app/classes/Helpers/AnimationConstants.java
@@ -0,0 +1,15 @@
+package com.mycompany.app.classes.Helpers;
+
+// AnimationConstants keeps all sprite image array constants together
+public class AnimationConstants {
+    public static class PlayerConstants {
+        public static int UP = 0;
+        public static int RIGHT = 1;
+        public static int DOWN = 2;
+        public static int LEFT = 3;
+    }
+
+    public static class EnemyConstants {
+
+    }
+}
