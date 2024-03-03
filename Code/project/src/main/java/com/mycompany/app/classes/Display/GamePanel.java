c5aa34971ca8fe525c79972e623653e3ded08d19
cas32 <cas32@sfu.ca>
2024-03-02 22:50:56 -0800

    created 2 methods for animating sprites in GamePanel, loadAnimations and updateAnimationTick.

--- Code/project/src/main/java/com/mycompany/app/classes/Display/GamePanel.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Display/GamePanel.java
@@ -20,7 +20,7 @@ public class GamePanel extends JPanel {
     final int scale = 3;
     final int tileSize = originalTileSize * scale;
 
-    // Grid size
+    // Maze size
     final int maxScreenCol = 16;
     final int maxScreenRow = 12;
 
@@ -28,30 +28,34 @@ public class GamePanel extends JPanel {
     final int screenWidth = tileSize * maxScreenCol;
     final int screenHeight = tileSize * maxScreenRow;
 
-    private BufferedImage img;
-
     private int xDelta = 0, yDelta = 0;
-    private int playerSpeed = 2;
-    private boolean moving = false;
-    
+    private BufferedImage img;
+    private BufferedImage[][] animations; // 2d image array of the images for player movements
+    private int animationTick, animationIndex, animationSpeed = 35;
+
     public GamePanel() {
         this.setBackground(Color.BLACK);
-        this.importImg();
-        this.setPanelSize();
-        this.addKeyListener(new KeyboardInputs(this));
+
+        importImg();
+        loadAnimations();
+
+        setPanelSize();
+        addKeyListener(new KeyboardInputs(this));
     }
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-        updatePos();
-        g2.drawImage(img.getSubimage(16 * 4, 24, 16, 24), xDelta, yDelta, tileSize, 72, null);
-        g2.dispose();
+        // g2.drawImage(img.getSubimage(16 * 4, 24, 16, 24), xDelta, yDelta, tileSize,
+        // 72, null);
+        updateAnimationTick();
+        g2.drawImage(animations[3][animationIndex], xDelta, yDelta, tileSize, 72, null);
     }
 
     private void importImg() {
-        // Source of player sprites: https://axulart.itch.io/small-8-direction-characters
+        // Source of player sprites:
+        // https://axulart.itch.io/small-8-direction-characters
         InputStream is = getClass().getResourceAsStream("player_sprites.png");
 
         try {
@@ -74,43 +78,40 @@ public class GamePanel extends JPanel {
         setMaximumSize(size);
     }
 
-    public void setMoving(boolean moving) {
-        this.moving = moving;
+    public void changeXDelta(int value) {
+        this.xDelta += value;
     }
 
-    /**
-     * Updates the player's position based on the currently pressed keys.
-     * If the player is moving, it adjusts the position according to the keys being pressed.
-     * If diagonal movement is detected, it normalizes the speed to maintain consistent movement.
-     */
-    private void updatePos() {
-        if (moving) {
-            if (KeyboardInputs.isUpPressed && KeyboardInputs.isRightPressed) {
-                xDelta += playerSpeed / Math.sqrt(2); // Move diagonally up-right
-                yDelta -= playerSpeed / Math.sqrt(2);
-            } else if (KeyboardInputs.isUpPressed && KeyboardInputs.isLeftPressed) {
-                xDelta -= playerSpeed / Math.sqrt(2); // Move diagonally up-left
-                yDelta -= playerSpeed / Math.sqrt(2);
-            } else if (KeyboardInputs.isDownPressed && KeyboardInputs.isRightPressed) {
-                xDelta += playerSpeed / Math.sqrt(2); // Move diagonally down-right
-                yDelta += playerSpeed / Math.sqrt(2);
-            } else if (KeyboardInputs.isDownPressed && KeyboardInputs.isLeftPressed) {
-                xDelta -= playerSpeed / Math.sqrt(2); // Move diagonally down-left
-                yDelta += playerSpeed / Math.sqrt(2);
-            } else {
-                if (KeyboardInputs.isUpPressed) {
-                    yDelta -= playerSpeed; // Move up
-                }
-                if (KeyboardInputs.isDownPressed) {
-                    yDelta += playerSpeed; // Move down
-                }
-                if (KeyboardInputs.isLeftPressed) {
-                    xDelta -= playerSpeed; // Move left
-                }
-                if (KeyboardInputs.isRightPressed) {
-                    xDelta += playerSpeed; // Move right
-                }
+    public void changeYDelta(int value) {
+        this.yDelta += value;
+    }
+
+    public int getTileSize() {
+        return tileSize;
+    }
+
+    // creates the Image array for the movement animations
+    private void loadAnimations() {
+        animations = new BufferedImage[4][3];
+        for (int j = 0; j < animations.length; j++) {
+            for (int i = 0; i < animations[j].length; i++) {
+                animations[j][i] = img.getSubimage(j * 32, 24 + (i * 24), 16, 24);
             }
         }
+
+    }
+
+    // updates the animation array during the game loop thread
+    private void updateAnimationTick() {
+
+        animationTick++;
+        if (animationTick >= animationSpeed) {
+            animationTick = 0;
+            animationIndex++;
+            if (animationIndex >= 3) {
+                animationIndex = 0;
+            }
+        }
+
     }
 }
