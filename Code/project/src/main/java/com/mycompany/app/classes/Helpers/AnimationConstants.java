4d0951195e916c1f8775880b90e9fa01956d8abc
cas32 <claire_shou@sfu.ca>
2024-03-03 19:58:18 -0800

    created a method PlayerSpriteAmount and adjusted PlayerDirection in gamePanel.

--- Code/project/src/main/java/com/mycompany/app/classes/Helpers/AnimationConstants.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Helpers/AnimationConstants.java
@@ -3,10 +3,15 @@ package com.mycompany.app.classes.Helpers;
 // AnimationConstants keeps all sprite image array constants together
 public class AnimationConstants {
     public static class PlayerConstants {
-        public static int UP = 0;
-        public static int RIGHT = 1;
-        public static int DOWN = 2;
-        public static int LEFT = 3;
+        public static final int UP = 0;
+        public static final int RIGHT = 1;
+        public static final int DOWN = 2;
+        public static final int LEFT = 3;
+    }
+
+    // returns the amount of animation sprites in an array
+    public static int PlayerSpriteAmount(int playerAction) {
+        return 3;
     }
 
     public static class EnemyConstants {
