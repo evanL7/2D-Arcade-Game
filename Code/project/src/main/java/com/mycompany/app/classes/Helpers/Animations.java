73da7db509c1036f32454cc49d913a6636871f9f
cas32 <claire_shou@sfu.ca>
2024-03-03 19:39:50 -0800

    created an Animation class to animate any entities

---
+++ Code/project/src/main/java/com/mycompany/app/classes/Helpers/Animations.java
@@ -0,0 +1,17 @@
+package com.mycompany.app.classes.Helpers;
+
+import java.awt.image.BufferedImage;
+
+// adds animations for enities
+public class Animations {
+    // ATTRIBUTES
+    private BufferedImage img;
+    private BufferedImage[][] animations; // 2d image array of the images for player movements
+
+    // CONSTRUCTOR
+    // param: takes the image of the sprites
+    public Animations(BufferedImage img) {
+
+    }
+
+}
