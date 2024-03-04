e943ea40ab8dbd23c6a442aa4ce36346542f8cd5
mza128 <mza128@sfu.ca>
2024-03-03 19:25:57 -0800

    basic start menu just for functionality for now, only start and exit work

--- Code/project/src/main/java/com/mycompany/app/classes/Display/GameMenu.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Display/GameMenu.java
@@ -1,24 +1,25 @@
+// GameMenu.java
 package com.mycompany.app.classes.Display;
 
 public class GameMenu {
-    
+
     // Function to start the game
     public void start() {
-        
+        // Instantiate the game and start it
+        Game game = new Game();
+        game.startGameLoop();
     }
-    
 
     public void customize() {
-        
+        // TODO: Implement customization logic
     }
-    
-   
+
     public void settings() {
-       
+        // TODO: Implement settings logic
     }
-    
-   
+
     public void exit() {
-       
+        // Exit the application
+        System.exit(0);
     }
 }
