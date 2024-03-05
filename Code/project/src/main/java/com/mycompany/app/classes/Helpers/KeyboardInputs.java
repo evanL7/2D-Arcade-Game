ea8c50271e9fd171dde571b1f6eea3da004af3c6
evan <eevanlaw@gmail.com>
2024-03-04 20:36:00 -0800

    updated KeyboardInputs class to use updated methods

--- Code/project/src/main/java/com/mycompany/app/classes/Helpers/KeyboardInputs.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Helpers/KeyboardInputs.java
@@ -19,15 +19,19 @@ public class KeyboardInputs implements KeyListener {
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
+                gamePanel.getGame().getPlayer().setUp(true);
                 gamePanel.getGame().getPlayer().setAction(PlayerConstants.UP);
                 break;
             case KeyEvent.VK_A:
+                gamePanel.getGame().getPlayer().setLeft(true);
                 gamePanel.getGame().getPlayer().setAction(PlayerConstants.LEFT);
                 break;
             case KeyEvent.VK_S:
+                gamePanel.getGame().getPlayer().setDown(true);
                 gamePanel.getGame().getPlayer().setAction(PlayerConstants.DOWN);
                 break;
             case KeyEvent.VK_D:
+                gamePanel.getGame().getPlayer().setRight(true);
                 gamePanel.getGame().getPlayer().setAction(PlayerConstants.RIGHT);
                 break;
         }
@@ -37,12 +41,18 @@ public class KeyboardInputs implements KeyListener {
     public void keyReleased(KeyEvent e) {
         switch (e.getKeyCode()) {
             case KeyEvent.VK_W:
-            case KeyEvent.VK_A:
-            case KeyEvent.VK_S:
-            case KeyEvent.VK_D:
-                gamePanel.getGame().getPlayer().setMoving(false);
-                break;
-        }
+			    gamePanel.getGame().getPlayer().setUp(false);
+			    break;
+		    case KeyEvent.VK_A:
+			    gamePanel.getGame().getPlayer().setLeft(false);
+			    break;
+		    case KeyEvent.VK_S:
+			    gamePanel.getGame().getPlayer().setDown(false);
+			    break;
+		    case KeyEvent.VK_D:
+			    gamePanel.getGame().getPlayer().setRight(false);
+			    break;
+		}
     }
 
     @Override
