cf34d9bef74cf94addd3ca838dbe7dace9946194
Evan <eevanlaw@gmail.com>
2024-03-04 21:02:05 -0800

    changed indentations from tabs to spaces

--- Code/project/src/main/java/com/mycompany/app/classes/MoveableEntity/Player.java
+++ Code/project/src/main/java/com/mycompany/app/classes/MoveableEntity/Player.java
@@ -109,9 +109,9 @@ public class Player extends MoveableEntity {
     }
 
     // GETTERS AND SETTERS
-	public void resetDirBooleans() {
-		left = false; right = false; up = false; down = false;		
-	}
+    public void resetDirBooleans() {
+        left = false; right = false; up = false; down = false;		
+    }
 
     public void setMoving(boolean moving) {
         this.moving = moving;
@@ -130,35 +130,35 @@ public class Player extends MoveableEntity {
         this.regularRewardsCollected = regularRewardsCollected;
     }
 
-	public boolean isLeft() {
-		return left;
-	}
+    public boolean isLeft() {
+        return left;
+    }
 
-	public void setLeft(boolean left) {
-		this.left = left;
-	}
+    public void setLeft(boolean left) {
+        this.left = left;
+    }
 
-	public boolean isUp() {
-		return up;
-	}
+    public boolean isUp() {
+        return up;
+    }
 
-	public void setUp(boolean up) {
-		this.up = up;
-	}
+    public void setUp(boolean up) {
+        this.up = up;
+    }
 
-	public boolean isRight() {
-		return right;
-	}
+    public boolean isRight() {
+        return right;
+    }
 
-	public void setRight(boolean right) {
-		this.right = right;
-	}
+    public void setRight(boolean right) {
+        this.right = right;
+    }
 
-	public boolean isDown() {
-		return down;
-	}
+    public boolean isDown() {
+        return down;
+    }
 
-	public void setDown(boolean down) {
-		this.down = down;
-	}
+    public void setDown(boolean down) {
+        this.down = down;
+    }
 }
