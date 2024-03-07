57cbdb9f34a989073fc819d34de2674ef3bc6858
ktc15 <tim_chou@sfu.ca>
2024-03-06 16:57:58 -0800

    Added Javadocs to improve readability

--- Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/Trap.java
+++ Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/Trap.java
@@ -6,18 +6,38 @@ import com.mycompany.app.classes.Helpers.Position;
 // trap sprite from https://bdragon1727.itch.io/free-trap-platformer
 // unsure of dimensions
 
+/**
+ * Represents a trap in the game.
+ *
+ * <p>Traps are static entities that cause damage upon collision.
+ * <p>Traps will only despawn if a player collides with them
+ */
 public class Trap extends StaticEntity {
 
+    /** The amount of damage the trap inflicts. */
     private float damage;
 
-    // constructor
-    // doesn't need despawnTimer as it would only despawn if collided with
+    /**
+     * Constructs a new Trap.
+     *
+     * @param position        The position of the trap.
+     * @param objectDespawns  Whether the trap despawns upon collision.
+     * @param sprite          The sprite representing the trap.
+     * @param damage          The amount of damage the trap inflicts.
+     */
     public Trap(Position position, boolean objectDespawns, Image sprite, float damage) {
+        // doesn't need despawnTimer as it would only despawn if collided with
         super(position, objectDespawns, sprite);
         this.damage = damage;
     }
 
     // Methods
+
+    /**
+     * Gets the amount of damage the trap inflicts.
+     *
+     * @return The amount of damage.
+     */
     public float getDamage() {
         return damage;
     }
