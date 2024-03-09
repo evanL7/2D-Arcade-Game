facc370466946b2b742e3857dc21db2010c92024
evan <eevanlaw@gmail.com>
2024-03-08 20:20:42 -0800

    added a Tile class that defines a tile on the game screen

---
+++ Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/Tile.java
@@ -0,0 +1,9 @@
+package com.mycompany.app.classes.StaticEntity;
+
+import java.awt.image.BufferedImage;
+
+public class Tile {
+    
+    public BufferedImage image;
+    public boolean collision = false;
+}
