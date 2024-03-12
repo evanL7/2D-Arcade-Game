f98b12b33401e5bc6444be80d1d5ec7664189e31
evan <eevanlaw@gmail.com>
2024-03-11 19:40:38 -0700

    refactored KeyboardInputs to prevent bug where player walks in walls and set default animation to down to appear idle

--- Code/project/src/main/java/com/mycompany/app/classes/Helpers/KeyboardInputs.java
+++ Code/project/src/main/java/com/mycompany/app/classes/Helpers/KeyboardInputs.java
@@ -2,60 +2,65 @@ package com.mycompany.app.classes.Helpers;
 
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.HashSet;
 
 import com.mycompany.app.classes.Display.GamePanel;
-
 import com.mycompany.app.classes.Helpers.AnimationConstants.PlayerConstants;
 
 public class KeyboardInputs implements KeyListener {
 
     private GamePanel gamePanel;
+    private HashSet<Integer> keysPressed;
 
     public KeyboardInputs(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
+        this.keysPressed = new HashSet<>();
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_W:
-                gamePanel.getGame().getPlayer().setUp(true);
-                gamePanel.getGame().getPlayer().setAction(PlayerConstants.UP);
-                break;
-            case KeyEvent.VK_A:
-                gamePanel.getGame().getPlayer().setLeft(true);
-                gamePanel.getGame().getPlayer().setAction(PlayerConstants.LEFT);
-                break;
-            case KeyEvent.VK_S:
-                gamePanel.getGame().getPlayer().setDown(true);
-                gamePanel.getGame().getPlayer().setAction(PlayerConstants.DOWN);
-                break;
-            case KeyEvent.VK_D:
-                gamePanel.getGame().getPlayer().setRight(true);
-                gamePanel.getGame().getPlayer().setAction(PlayerConstants.RIGHT);
-                break;
+        int keyCode = e.getKeyCode();
+        if (!keysPressed.contains(keyCode)) {
+            keysPressed.add(keyCode);
+            handleKeys();
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        switch (e.getKeyCode()) {
-            case KeyEvent.VK_W:
-			    gamePanel.getGame().getPlayer().setUp(false);
-			    break;
-		    case KeyEvent.VK_A:
-			    gamePanel.getGame().getPlayer().setLeft(false);
-			    break;
-		    case KeyEvent.VK_S:
-			    gamePanel.getGame().getPlayer().setDown(false);
-			    break;
-		    case KeyEvent.VK_D:
-			    gamePanel.getGame().getPlayer().setRight(false);
-			    break;
-		}
+        int keyCode = e.getKeyCode();
+        keysPressed.remove(keyCode);
+        handleKeys();
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
+    }
+
+    private void handleKeys() {
+        // Handle keys based on the current state of keysPressed
+        boolean up = keysPressed.contains(KeyEvent.VK_W);
+        boolean left = keysPressed.contains(KeyEvent.VK_A);
+        boolean down = keysPressed.contains(KeyEvent.VK_S);
+        boolean right = keysPressed.contains(KeyEvent.VK_D);
+        
+        // Set player actions based on the keys pressed
+        gamePanel.getGame().getPlayer().setUp(up);
+        gamePanel.getGame().getPlayer().setLeft(left);
+        gamePanel.getGame().getPlayer().setDown(down);
+        gamePanel.getGame().getPlayer().setRight(right);
+
+        // Determine the primary direction based on the last key pressed
+        int action = PlayerConstants.DOWN; // Default action
+        if (up) {
+            action = PlayerConstants.UP;
+        } else if (left) {
+            action = PlayerConstants.LEFT;
+        } else if (down) {
+            action = PlayerConstants.DOWN;
+        } else if (right) {
+            action = PlayerConstants.RIGHT;
+        }
+        gamePanel.getGame().getPlayer().setAction(action);
     }
 }
