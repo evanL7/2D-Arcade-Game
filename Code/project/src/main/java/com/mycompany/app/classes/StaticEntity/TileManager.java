861417c9f230ed9dd32c3c2caf952be851941b22
evan <eevanlaw@gmail.com>
2024-03-12 16:49:50 -0700

    moved png files to assets to improve file management

--- Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/TileManager.java
+++ Code/project/src/main/java/com/mycompany/app/classes/StaticEntity/TileManager.java
@@ -38,58 +38,58 @@ public class TileManager {
         try {
             // Load the tile sprites
             tile[0] = new Tile();
-            tile[0].image = ImageIO.read(getClass().getResourceAsStream("tileFloor.png"));
+            tile[0].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileFloor.png"));
 
             tile[1] = new Tile();
-            tile[1].image = ImageIO.read(getClass().getResourceAsStream("tileWallN.png"));
+            tile[1].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileWallN.png"));
             tile[1].collision = true;
 
             tile[2] = new Tile();
-            tile[2].image = ImageIO.read(getClass().getResourceAsStream("tileWallS.png"));
+            tile[2].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileWallS.png"));
             tile[2].collision = true;
 
             tile[3] = new Tile();
-            tile[3].image = ImageIO.read(getClass().getResourceAsStream("tileWallE.png"));
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileWallE.png"));
             tile[3].collision = true;
 
             tile[4] = new Tile();
-            tile[4].image = ImageIO.read(getClass().getResourceAsStream("tileWallW.png"));
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileWallW.png"));
             tile[4].collision = true;
 
             tile[5] = new Tile();
-            tile[5].image = ImageIO.read(getClass().getResourceAsStream("tileCornerNW.png"));
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileCornerNW.png"));
             tile[5].collision = true;
 
             tile[6] = new Tile();
-            tile[6].image = ImageIO.read(getClass().getResourceAsStream("tileCornerNE.png"));
+            tile[6].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileCornerNE.png"));
             tile[6].collision = true;
 
             tile[7] = new Tile();
-            tile[7].image = ImageIO.read(getClass().getResourceAsStream("tileCornerSW.png"));
+            tile[7].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileCornerSW.png"));
             tile[7].collision = true;
 
             tile[8] = new Tile();
-            tile[8].image = ImageIO.read(getClass().getResourceAsStream("tileCornerSE.png"));
+            tile[8].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileCornerSE.png"));
             tile[8].collision = true;
 
             tile[9] = new Tile();
-            tile[9].image = ImageIO.read(getClass().getResourceAsStream("tileRoomWall.png"));
+            tile[9].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileRoomWall.png"));
             tile[9].collision = true;
 
             tile[10] = new Tile();
-            tile[10].image = ImageIO.read(getClass().getResourceAsStream("tileRoomWallBotMid.png"));
+            tile[10].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileRoomWallBotMid.png"));
             tile[10].collision = true;
 
             tile[11] = new Tile();
-            tile[11].image = ImageIO.read(getClass().getResourceAsStream("tileRoomWallBotRightCorner.png"));
+            tile[11].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileRoomWallBotRightCorner.png"));
             tile[11].collision = true;
 
             tile[12] = new Tile();
-            tile[12].image = ImageIO.read(getClass().getResourceAsStream("tileRoomWallTopMid.png"));
+            tile[12].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileRoomWallTopMid.png"));
             tile[12].collision = true;
 
             tile[13] = new Tile();
-            tile[13].image = ImageIO.read(getClass().getResourceAsStream("tileRoomWallTopRightCorner.png"));
+            tile[13].image = ImageIO.read(getClass().getResourceAsStream("/assets/tileRoomWallTopRightCorner.png"));
             tile[13].collision = true;
 
         } catch (Exception e) {
