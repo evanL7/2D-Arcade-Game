3f9d1c1b3c18891c5bbb3dacc873782441cbc4ce
evan <eevanlaw@gmail.com>
2024-03-14 12:54:29 -0700

    separated initializing gamestates, updating, and rendering to their respective classes

--- Code/project/src/main/java/Display/Game.java
+++ Code/project/src/main/java/Display/Game.java
@@ -3,6 +3,9 @@ package Display;
 import java.awt.Graphics;
 import java.awt.font.GraphicAttribute;
 
+import Gamestates.Gamestate;
+import Gamestates.Menu;
+import Gamestates.Playing;
 import Helpers.CollisionChecker;
 import Helpers.Position;
 import Helpers.AnimationConstants.PlayerConstants;
@@ -35,28 +38,8 @@ public class Game implements Runnable {
     public final static int screenWidth = tileSize * maxScreenCol;
     public final static int screenHeight = tileSize * maxScreenRow;
 
-    private TileManager tileManager;
-    public CollisionChecker collisionChecker;
-    private Player player;
-    private Enemy enemy;
-    private Trap trap;
-    private Reward rewardReg; // add
-    private Reward rewardBonus; // add
-
-    int playerX = Game.screenWidth / 2 - Game.tileSize / 2;
-    int playerY = Game.screenHeight / 2 - Game.tileSize / 2;
-
-    int enemyX = Game.screenWidth / 3 - Game.tileSize / 2;
-    int enemyY = Game.screenHeight / 3 - Game.tileSize / 2;
-
-    int trapX = Game.screenWidth / 2 - Game.tileSize / 2;
-    int trapY = Game.screenHeight / 3 - Game.tileSize / 2;
-
-    int regRewardX = Game.screenWidth / 4 - Game.tileSize / 4; // add
-    int regRewardY = Game.screenHeight / 4 - Game.tileSize / 4; // add
-
-    int bonusRX = Game.screenWidth / 2 - Game.tileSize / 3; // add
-    int bonusRY = Game.screenHeight / 4 - Game.tileSize / 5; // add
+    private Playing playing;
+    private Menu menu;
 
     public Game() {
         initClasses();
@@ -65,22 +48,12 @@ public class Game implements Runnable {
         gameWindow = new GameWindow(gamePanel);
         gamePanel.requestFocus();
 
-        // Create the Camera object with the player
-        camera = new Camera(player);
-
         startGameLoop();
     }
 
     private void initClasses() {
-        tileManager = new TileManager();
-        collisionChecker = new CollisionChecker(tileManager);
-        player = new Player(new Position(playerX, playerY), collisionChecker);
-        enemy = new Enemy(new Position(enemyX, enemyY));
-        trap = new Trap(new Position(trapX, trapY), 1);
-        rewardReg = new Reward(new Position(regRewardX, regRewardY), 10, 1); // add
-
-        // this takes approx 25 seconds to despawn from the screen
-        rewardBonus = new Reward(new Position(bonusRX, bonusRY), 10000, 10, 1); // add
+        menu = new Menu(this);
+        playing = new Playing(this);
     }
 
     public void startGameLoop() {
@@ -89,41 +62,38 @@ public class Game implements Runnable {
     }
 
     private void update() {
-        player.update();
-        enemy.update(player.getPosition());
-        trap.update();
-        rewardReg.update();
-        if (rewardBonus != null) {
-            rewardBonus.update();
-            if (rewardBonus.getDespawnTimer() <= 0) {
-                rewardBonus = null;
-            }
-        }
 
+        switch (Gamestate.state) {
+            case MENU:
+                menu.update();
+                break;
+            case PLAYING:
+                playing.update();
+                break;
+            case OPTIONS:
+                break;
+            case QUIT:
+                break;
+        }
     }
 
     public void render(Graphics g) {
-        // Translate graphics object to adjust for camera position
-        camera.update();
-        camera.translate(g);
 
-        // Render game objects with adjusted coordinates
-        tileManager.draw(g);
-        // Adjust player's position based on camera
-        int playerRenderX = player.getPosition().getX() - camera.getXOffset();
-        int playerRenderY = player.getPosition().getY() - camera.getYOffset();
-        player.render(g);
-        enemy.render(g);
-        trap.render(g);
-        rewardReg.render(g); // add
-        if (rewardBonus != null && rewardBonus.getDespawnTimer() > 0) // add
-        {
-            rewardBonus.render(g); // add
+        switch (Gamestate.state) {
+            case MENU:
+                menu.draw(g);
+                break;
+            case PLAYING:
+                playing.draw(g);
+
+                break;
+            case OPTIONS:
+                break;
+            case QUIT:
+                break;
         }
 
-        // Reset graphics translation
-        camera.reset(g);
-        g.dispose();
+
     }
 
     @Override
@@ -170,10 +140,16 @@ public class Game implements Runnable {
     }
 
     public void windowFocusLost() {
-        player.resetDirBooleans();
+		if (Gamestate.state == Gamestate.PLAYING) {
+			playing.getPlayer().resetDirBooleans();
+        }
     }
 
-    public Player getPlayer() {
-        return player;
+    public Menu getMenu() {
+        return menu;
+    }
+
+    public Playing getPlaying() {
+        return playing;
     }
 }
