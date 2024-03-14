dc665d2bcb42d961873bddb2b116974d38ce2b7b
evan <eevanlaw@gmail.com>
2024-03-14 12:52:09 -0700

    updated main class

--- Code/project/src/main/java/Main.java
+++ Code/project/src/main/java/Main.java
@@ -1,50 +1,7 @@
-// package com.mycompany.app.classes;
-
-// import com.mycompany.app.classes.Display.Game;
-
-// public class Main {
-//     public static void main(String[] args) {
-//         // Displays game window
-//         new Game();
-//     }
-
-// }
-
-// Main.java
-
-
-import javax.swing.*;
-
-import Display.GameMenu;
-import Display.GameWindow;
+import Display.Game;
 
 public class Main {
     public static void main(String[] args) {
-        // Create an instance of the game menu
-        GameMenu gameMenu = new GameMenu();
-
-        // Display menu screen
-        int choice = JOptionPane.showOptionDialog(null, "Choose an option", "Menu", JOptionPane.DEFAULT_OPTION, JOptionPane.PLAIN_MESSAGE, null,
-                new String[]{"Start", "Customize", "Settings", "Exit"}, "Start");
-
-        // Perform action based on user choice
-        switch (choice) {
-            case 0: // Start
-                gameMenu.start();
-                break;
-            case 1: // Customize
-                gameMenu.customize();
-                break;
-            case 2: // Settings
-                gameMenu.settings();
-                break;
-            case 3: // Exit
-                gameMenu.exit();
-                break;
-            default:
-                // Invalid choice, do nothing or handle as needed
-                break;
-        }
+        new Game();
     }
 }
-
