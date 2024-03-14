222926c60007846e7c1e94e214c5180d9fb6bd7c
evan <eevanlaw@gmail.com>
2024-03-14 12:52:58 -0700

    moved reading user keyboard movements to the Playing class

--- Code/project/src/main/java/Helpers/KeyboardInputs.java
+++ Code/project/src/main/java/Helpers/KeyboardInputs.java
@@ -5,62 +5,47 @@ import java.awt.event.KeyListener;
 import java.util.HashSet;
 
 import Display.GamePanel;
+import Gamestates.Gamestate;
 import Helpers.AnimationConstants.PlayerConstants;
 
 public class KeyboardInputs implements KeyListener {
 
     private GamePanel gamePanel;
-    private HashSet<Integer> keysPressed;
 
     public KeyboardInputs(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
-        this.keysPressed = new HashSet<>();
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        if (!keysPressed.contains(keyCode)) {
-            keysPressed.add(keyCode);
-            handleKeys();
-        }
+        switch (Gamestate.state) {
+            case MENU:
+                gamePanel.getGame().getMenu().keyPressed(e);
+                break;
+            case PLAYING:
+                gamePanel.getGame().getPlaying().keyPressed(e);
+                break;
+            default:
+                break;
+            }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-        int keyCode = e.getKeyCode();
-        keysPressed.remove(keyCode);
-        handleKeys();
+        switch (Gamestate.state) {
+            case MENU:
+                gamePanel.getGame().getMenu().keyReleased(e);
+                break;
+            case PLAYING:
+                gamePanel.getGame().getPlaying().keyReleased(e);
+                break;
+            default:
+                break;
+
+        }
     }
 
     @Override
     public void keyTyped(KeyEvent e) {
-    }
-
-    private void handleKeys() {
-        // Handle keys based on the current state of keysPressed
-        boolean up = keysPressed.contains(KeyEvent.VK_W);
-        boolean left = keysPressed.contains(KeyEvent.VK_A);
-        boolean down = keysPressed.contains(KeyEvent.VK_S);
-        boolean right = keysPressed.contains(KeyEvent.VK_D);
-        
-        // Set player actions based on the keys pressed
-        gamePanel.getGame().getPlayer().setUp(up);
-        gamePanel.getGame().getPlayer().setLeft(left);
-        gamePanel.getGame().getPlayer().setDown(down);
-        gamePanel.getGame().getPlayer().setRight(right);
-
-        // Determine the primary direction based on the last key pressed
-        int action = gamePanel.getGame().getPlayer().getAction(); // Default action
-        if (up) {
-            action = PlayerConstants.UP;
-        } else if (left) {
-            action = PlayerConstants.LEFT;
-        } else if (down) {
-            action = PlayerConstants.DOWN;
-        } else if (right) {
-            action = PlayerConstants.RIGHT;
-        }
-        gamePanel.getGame().getPlayer().setAction(action);
     }
 }
