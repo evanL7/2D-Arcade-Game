300cc1997f22e4e54ab95e899553f354377378dc
evan <eevanlaw@gmail.com>
2024-03-14 12:51:29 -0700

    created a MouseInputs class for reading user mouse clicks

---
+++ Code/project/src/main/java/Helpers/MouseInputs.java
@@ -0,0 +1,56 @@
+package Helpers;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import Display.GamePanel;
+import Gamestates.Gamestate;
+
+public class MouseInputs implements MouseListener, MouseMotionListener {
+
+    private GamePanel gamePanel;
+
+    public MouseInputs(GamePanel gamePanel) {
+        this.gamePanel = gamePanel;
+    }
+
+    @Override
+    public void mouseDragged(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseMoved(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+		switch (Gamestate.state) {
+            case MENU:
+                gamePanel.getGame().getMenu().mouseClicked(e);
+                break;
+            case PLAYING:
+                gamePanel.getGame().getPlaying().mouseClicked(e);
+                break;
+            default:
+                break;    
+            }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+    }
+
+}
