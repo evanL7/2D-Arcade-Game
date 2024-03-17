5d07a1c09802004e60fc8006e66b81d775e478e3
cas32 <claire_shou@sfu.ca>
2024-03-16 22:10:22 -0700

    created a new method for checking Collisions

--- Code/project/src/main/java/MoveableEntity/MoveableEntity.java
+++ Code/project/src/main/java/MoveableEntity/MoveableEntity.java
@@ -2,18 +2,25 @@ package MoveableEntity;
 
 import java.awt.*;
 
+import Display.Game;
+import Gamestates.Playing;
 import Helpers.Position;
 
 public abstract class MoveableEntity {
     // ATTRIBUTES
     protected Image sprite;
     public Position position;
+    Playing playing;
     public int speed;
     protected boolean moving = false;
 
     public Rectangle solidArea;
     public boolean collisionOn = false;
     public boolean onPath = false;
+
+    // way to make this public from playing??
+    public int worldX = Game.tileSize * 23;
+    public int worldY = Game.tileSize * 21;
 
     /**
      * Constructs a MoveableEntity with the given position.
@@ -27,6 +34,10 @@ public abstract class MoveableEntity {
     // gets and returns the entities position
     public Position getPosition() {
         return this.position;
+    }
+
+    public void checkCollision() {
+        // TO DO
     }
 
 }
