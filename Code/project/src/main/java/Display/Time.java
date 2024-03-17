89b8eb20e2f6af1d5f599301c1a247d2979b1e2a
ktc15 <tim_chou@sfu.ca>
2024-03-16 17:46:59 -0700

    Added getter and relocated the calculations for timeElapsed

--- Code/project/src/main/java/Display/Time.java
+++ Code/project/src/main/java/Display/Time.java
@@ -9,11 +9,13 @@ public class Time {
     public long startTime; // when the timer initially started
     private long pausedTime; // when the timer was paused
     private long totalPausedTime; // how long the timer has been paused
+    private String timeElapsedStr; // the string representation of time elapsed
 
     public Time() {
         startTime = System.currentTimeMillis();
         pausedTime = 0;
         totalPausedTime = 0;
+        timeElapsedStr = null;
         
         // since the user sees the main menu first so timer should be paused
         pauseTimer(); 
@@ -34,7 +36,7 @@ public class Time {
         System.out.println("Game unpaused and the total time paused is: " + totalPausedTime); // test
     }
 
-    public void displayElapsedTime(Graphics g) {
+    private void calculateElapsedTime() {
         long currentTime = System.currentTimeMillis();
         long elapsedTime = currentTime - startTime - totalPausedTime;
 
@@ -42,10 +44,22 @@ public class Time {
         long minutes = (elapsedTime / 1000) / 60;
         long seconds = (elapsedTime / 1000) % 60;
 
+        // converts the time to a string
+        timeElapsedStr = String.format("%02d:%02d", minutes, seconds);
+    }
+    
+    // Getter
+    public String getElapsedTime() {
+        return timeElapsedStr;
+    }
+
+    public void displayElapsedTime(Graphics g) { 
+        calculateElapsedTime();
+        String elapsedTime = getElapsedTime();
+
         // Display elapsed time in the top right corner
-        String timeString = String.format("%02d:%02d", minutes, seconds);
         g.setColor(Color.RED);
         g.setFont(new Font("Arial", Font.BOLD, 20));
-        g.drawString("Elapsed Time: " + timeString, Game.screenWidth - 200, 30);
+        g.drawString("Elapsed Time: " + elapsedTime, Game.screenWidth - 200, 30);
     }
 }
