11ea535e5c7862dff8890c3337cac31d62bc2820
evan <eevanlaw@gmail.com>
2024-03-17 22:01:00 -0700

    added traps to the map

--- Code/project/src/main/java/Helpers/AssetManager.java
+++ Code/project/src/main/java/Helpers/AssetManager.java
@@ -19,5 +19,8 @@ public class AssetManager {
 
     public void setObjects() {
         playing.staticEntities[0] = new Door(new Position(23 * Game.tileSize, 23 *  Game.tileSize));
+        playing.staticEntities[1] = new Trap(new Position(9 * Game.tileSize, 3 *  Game.tileSize), 1);
+        playing.staticEntities[2] = new Trap(new Position(9 * Game.tileSize, 4 *  Game.tileSize), 1);
+        playing.staticEntities[3] = new Trap(new Position(9 * Game.tileSize, 5 *  Game.tileSize), 1);
     }
 }
