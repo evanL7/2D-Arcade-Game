201b14f40d4b5605689b191db7dc06036d3ed867
evan <eevanlaw@gmail.com>
2024-03-17 22:05:24 -0700

    referenced source of door sprite

--- Code/project/src/main/java/StaticEntity/Door.java
+++ Code/project/src/main/java/StaticEntity/Door.java
@@ -13,6 +13,12 @@ import Display.Game;
 import Helpers.ImageUtils;
 import Helpers.Position;
 
+
+/**
+ * Represents a door in the game.
+ * Used to transition between levels.
+ * Source of door sprite: https://www.youtube.com/watch?v=xYtXz34IJdY&list=PL_QPQmz5C6WUF-pOQDsbsKbaBZqXj4qSq
+ */
 public class Door extends StaticEntity {
 
     private BufferedImage doorImage;
