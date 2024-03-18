6e2e68f4b856cd2ff4438ed6fa081cbd04965e37
cas32 <claire_shou@sfu.ca>
2024-03-17 23:33:12 -0700

    put traps and grad caps into their positions onto the map

--- Code/project/src/main/java/Helpers/AssetManager.java
+++ Code/project/src/main/java/Helpers/AssetManager.java
@@ -11,7 +11,7 @@ import StaticEntity.Trap;
  * It sets the objects in the game.
  */
 public class AssetManager {
-    
+
     Playing playing;
 
     public AssetManager(Playing playing) {
@@ -19,12 +19,16 @@ public class AssetManager {
     }
 
     public void setObjects() {
-        playing.staticEntities[0] = new Door(new Position(23 * Game.tileSize, 23 *  Game.tileSize));
-        playing.staticEntities[1] = new Trap(new Position(9 * Game.tileSize, 3 *  Game.tileSize), 1);
-        playing.staticEntities[2] = new Trap(new Position(9 * Game.tileSize, 4 *  Game.tileSize), 1);
-        playing.staticEntities[3] = new Trap(new Position(9 * Game.tileSize, 5 *  Game.tileSize), 1);
-        playing.staticEntities[4] = new Reward(new Position(10 * Game.tileSize, 3 *  Game.tileSize), 1, 1);
-        playing.staticEntities[5] = new Reward(new Position(10 * Game.tileSize, 4 *  Game.tileSize), 1, 1);
-        playing.staticEntities[6] = new Reward(new Position(10 * Game.tileSize, 5 *  Game.tileSize), 1, 1);
+        playing.staticEntities[0] = new Door(new Position(23 * Game.tileSize, 23 * Game.tileSize));
+        playing.staticEntities[1] = new Trap(new Position(9 * Game.tileSize, 9 * Game.tileSize), 1);
+        playing.staticEntities[2] = new Trap(new Position(19 * Game.tileSize, 4 * Game.tileSize), 1);
+        playing.staticEntities[3] = new Trap(new Position(9 * Game.tileSize, 12 * Game.tileSize), 1);
+        playing.staticEntities[4] = new Trap(new Position(4 * Game.tileSize, 18 * Game.tileSize), 1);
+        playing.staticEntities[5] = new Trap(new Position(9 * Game.tileSize, 20 * Game.tileSize), 1);
+        playing.staticEntities[6] = new Trap(new Position(18 * Game.tileSize, 12 * Game.tileSize), 1);
+        playing.staticEntities[7] = new Trap(new Position(15 * Game.tileSize, 16 * Game.tileSize), 1);
+        playing.staticEntities[8] = new Reward(new Position(5 * Game.tileSize, 21 * Game.tileSize), 1, 1);
+        playing.staticEntities[9] = new Reward(new Position(20 * Game.tileSize, 11 * Game.tileSize), 1, 1);
+        playing.staticEntities[10] = new Reward(new Position(21 * Game.tileSize, 4 * Game.tileSize), 1, 1);
     }
 }
