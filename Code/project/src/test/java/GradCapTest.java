2eceb98f63ca11dea6c22b277a46e6ad122980ba
cas32 <claire_shou@sfu.ca>
2024-03-30 19:29:49 -0700

    adjusted the GradCapTest file to set up game before each test instead of creating new objects every test

--- Code/project/src/test/java/GradCapTest.java
+++ Code/project/src/test/java/GradCapTest.java
@@ -1,4 +1,6 @@
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Display.Game;
@@ -11,24 +13,36 @@ import StaticEntity.Reward;
 import StaticEntity.TileManager;
 
 public class GradCapTest {
+    private Player player;
+    private Game game;
+    private Playing playing;
+    private TileManager tileManager;
+    private CollisionChecker collisionChecker;
+    private Score scoreObject;
+
+    @BeforeEach
+    void setUp() {
+        game = new Game();
+        playing = new Playing(game);
+        tileManager = new TileManager(playing);
+        collisionChecker = new CollisionChecker(tileManager);
+        scoreObject = new Score();
+    }
 
     @Test
     public void testCheckPlayerRewardCollision() {
-        Game game = new Game();
-        Playing playing = new Playing(game);
-        TileManager tileManager = new TileManager(playing);
-        CollisionChecker collisionChecker = new CollisionChecker(tileManager);
-        Score scoreObject = new Score();
+
         assertTrue(scoreObject.getScore() == 2);
-        Player player = new Player(new Position(2 * Game.tileSize, 3 * Game.tileSize), collisionChecker, playing,
+        player = new Player(new Position(2 * Game.tileSize, 3 * Game.tileSize), collisionChecker, playing,
                 scoreObject);
 
         Reward reward = new Reward(new Position(2 * Game.tileSize, 3 * Game.tileSize), 0.5f, 1);
 
         Boolean result = collisionChecker.checkPlayerRewardCollision(player, reward);
         double score = player.getScoreObj().getScore();
-        
+
         assertTrue(result);
         assertEquals(2.5, score);
     }
+
 }
