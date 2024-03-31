42fe0484eebe934d701d95d8bee58c7aae8e728d
cas32 <claire_shou@sfu.ca>
2024-03-30 19:40:27 -0700

    added javadocs to the new setPosition method

--- Code/project/src/main/java/MoveableEntity/MoveableEntity.java
+++ Code/project/src/main/java/MoveableEntity/MoveableEntity.java
@@ -119,6 +119,12 @@ public abstract class MoveableEntity {
         moveableEntities = new ArrayList<>();
     }
 
+    /**
+     * Sets the entities position attribute
+     * 
+     * @param x the x coordinate
+     * @param y the y coordinate
+     */
     public void setPosition(int x, int y) {
         position.setX(x);
         position.setY(y);
