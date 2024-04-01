7cf6f7b391e0a1f5a25bb4e56150a60004d797e2
ktc15 <tim_chou@sfu.ca>
2024-03-31 18:42:54 -0700

    Modified existing test cases to also test normal movement without collision

--- Code/project/src/test/java/PlayerTest.java
+++ Code/project/src/test/java/PlayerTest.java
@@ -53,117 +53,143 @@ public class PlayerTest {
     }
 
     @Test
-    public void checkRightWallCollision() {
+    public void checkRightMovement() {
         
         // spawn the player right next to the right wall
         // want to check if going right changes the player's position at all
-        player = new Player(new Position(1107, 3 * Game.tileSize), collisionChecker, playing, scoreObject);
+        player = new Player(new Position(1106, 3 * Game.tileSize), collisionChecker, playing, scoreObject);
         
         // Simulate pressing the "D" key to go right
+        // player should be able to move 1 to the right
         player.setRight(true);
         player.setAction(1);
         assertEquals(1, player.getAction());
-
-        // Check if the right variable is set to true
         assertTrue(player.isRight());
+
+        // check if player moved 1 right
+        player.update();
+        assertEquals(1107, player.getPosition().getX());
+        
     
+        // now player should be against the right wall so movement shouldn't be possible
         Position oldPos = new Position(player.getPosition().getX(), player.getPosition().getY());
-        //System.out.println("OldPos is: " + oldPos.toString());
+
+        player.setRight(true);
+        player.setAction(1);
         
         player.update();
         player.update();
         player.update();
 
         Position newPos = new Position(player.getPosition().getX(), player.getPosition().getY());
-        //System.out.println("NewPos is: " + newPos.toString());
         
+        // checks if the position changed
+        // it shouldn't change since there is a wall in the way
         assertTrue(comparePos(oldPos, newPos));
         player.resetDirBooleans();
     }
 
     @Test
-    public void checkLeftWallCollision() {
+    public void checkLeftMovement() {
         
         // spawn the player right next to the left wall
         // want to check if going left changes the player's position at all
-        player = new Player(new Position(40, 3 * Game.tileSize), collisionChecker, playing, scoreObject);
+        player = new Player(new Position(41, 3 * Game.tileSize), collisionChecker, playing, scoreObject);
         
         // Simulate pressing the "A" key to go left
+        // player should be able to move 1 to the left
         player.setLeft(true);
         player.setAction(3);
-
-        // Check if the left variable is set to true
         assertTrue(player.isLeft());
         assertEquals(3, player.getAction());
-    
+        
+        // check if player moved 1 left
+        player.update();
+        assertEquals(40, player.getPosition().getX());
+        
+        player.setLeft(true);
+        player.setAction(3);
+        // now player should be against the left wall so movement shouldn't be possible
         Position oldPos = new Position(player.getPosition().getX(), player.getPosition().getY());
-        //System.out.println("OldPos is: " + oldPos.toString());
         
         player.update();
         player.update();
         player.update();
 
         Position newPos = new Position(player.getPosition().getX(), player.getPosition().getY());
-        //System.out.println("NewPos is: " + newPos.toString());
         
+        // checks if the position changed
+        // it shouldn't change since there is a wall in the way
         assertTrue(comparePos(oldPos, newPos));
         player.resetDirBooleans();
     }
 
     @Test
-    public void checkTopWallCollision() {
+    public void checkUpMovement() {
         
         // spawn the player right under the top wall
         // want to check if going up changes the player's position at all
-        player = new Player(new Position(2 * Game.tileSize, 128), collisionChecker, playing, scoreObject);
+        player = new Player(new Position(2 * Game.tileSize, 129), collisionChecker, playing, scoreObject);
         
         // Simulate pressing the "W" key to go up
+        // player should be able to move 1 up
         player.setUp(true);
         player.setAction(0);
-
-        // Check if the left variable is set to true
         assertTrue(player.isUp());
         assertEquals(0, player.getAction());
+
+        // check if player moved 1 up
+        player.update();
+        assertEquals(128, player.getPosition().getY());
     
+        player.setUp(true);
+        player.setAction(0);
+        // now player should be against the top wall so movement shouldn't be possible
         Position oldPos = new Position(player.getPosition().getX(), player.getPosition().getY());
-        //System.out.println("OldPos is: " + oldPos.toString());
         
         player.update();
         player.update();
         player.update();
 
         Position newPos = new Position(player.getPosition().getX(), player.getPosition().getY());
-        //System.out.println("NewPos is: " + newPos.toString());
         
+        // checks if the position changed
+        // it shouldn't change since there is a wall in the way
         assertTrue(comparePos(oldPos, newPos));
         player.resetDirBooleans();
     }
 
     @Test
-    public void checkBottomWallCollision() {
+    public void checkDownMovement() {
         
         // spawn the player right above the bottom wall
         // want to check if going down changes the player's position at all
-        player = new Player(new Position(2 * Game.tileSize, 1087), collisionChecker, playing, scoreObject);
+        player = new Player(new Position(2 * Game.tileSize, 1086), collisionChecker, playing, scoreObject);
         
         // Simulate pressing the "S" key to go down
+        // player should be able to move 1 down
         player.setDown(true);
         player.setAction(2);
-
-        // Check if the left variable is set to true
         assertTrue(player.isDown());
         assertEquals(2, player.getAction());
-    
+
+        // check if player moved 1 down
+        player.update();
+        assertEquals(1087, player.getPosition().getY());
+
+        player.setDown(true);
+        player.setAction(2);
+        // now player should be against the bottom wall so movement shouldn't be possible
         Position oldPos = new Position(player.getPosition().getX(), player.getPosition().getY());
-        //System.out.println("OldPos is: " + oldPos.toString());
         
         player.update();
         player.update();
         player.update();
 
         Position newPos = new Position(player.getPosition().getX(), player.getPosition().getY());
-        //System.out.println("NewPos is: " + newPos.toString());
         
+        // checks if the position changed
+        // it shouldn't change since there is a wall in the way
         assertTrue(comparePos(oldPos, newPos));
         player.resetDirBooleans();
     }
