3dab1649f612f50816d6c2be4d0abc868e37a09f
cas32 <claire_shou@sfu.ca>
2024-04-02 22:11:54 -0700

    created a Trap Test which checks collision with multiple traps and the score

--- Code/project/src/test/java/TrapTest.java
+++ Code/project/src/test/java/TrapTest.java
@@ -11,42 +11,76 @@ import MoveableEntity.Player;
 import StaticEntity.TileManager;
 import StaticEntity.Trap;
 
-
 public class TrapTest {
-    
+
     private Player player;
     private Game game;
     private Playing playing;
     private TileManager tileManager;
     private CollisionChecker collisionChecker;
-    private Trap trap;
     private Score scoreObject;
 
     @BeforeEach
     void setUp() {
         game = new Game();
         playing = new Playing(game);
-        tileManager = new TileManager(playing);      
+        tileManager = new TileManager(playing);
         collisionChecker = new CollisionChecker(tileManager);
-        scoreObject = new Score();  
+        scoreObject = new Score();
     }
 
     @Test
     public void testDamage() {
-        
+
         player = new Player(new Position(2 * Game.tileSize, 3 * Game.tileSize), collisionChecker, playing, scoreObject);
-        trap = new Trap(new Position(2 * Game.tileSize, 3 * Game.tileSize));
+        Trap trap = new Trap(new Position(2 * Game.tileSize, 3 * Game.tileSize));
 
         double originalScore = player.getScoreObj().getScore();
         assertTrue(originalScore == 2);
-        
+
         Boolean result = collisionChecker.checkPlayerTrapCollision(player, trap);
-        double actualScore = player.getScoreObj().getScore();        
+        double actualScore = player.getScoreObj().getScore();
         double expectedScore = originalScore - trap.getDamage();
 
         assertTrue(result);
         assertEquals(expectedScore, actualScore);
-        
+
     }
-    
+
+    @Test
+    public void testMultipleTrapPlayerCollisions() {
+        player = new Player(new Position(2 * Game.tileSize, 3 * Game.tileSize), collisionChecker, playing, scoreObject);
+        Trap trap1 = new Trap(new Position(2 * Game.tileSize, 3 * Game.tileSize));
+        Trap trap2 = new Trap(new Position(4 * Game.tileSize, 5 * Game.tileSize));
+        Trap trap3 = new Trap(new Position(6 * Game.tileSize, 7 * Game.tileSize));
+        Trap trap4 = new Trap(new Position(2 * Game.tileSize, 5 * Game.tileSize));
+        Trap trap5 = new Trap(new Position(6 * Game.tileSize, 3 * Game.tileSize));
+        Trap trap6 = new Trap(new Position(4 * Game.tileSize, 3 * Game.tileSize));
+
+        double originalScore = player.getScoreObj().getScore();
+        assertTrue(originalScore == 2);
+
+        collisionChecker.checkPlayerTrapCollision(player, trap1);
+
+        player.setPosition(4 * Game.tileSize, 5 * Game.tileSize);
+        collisionChecker.checkPlayerTrapCollision(player, trap2);
+
+        player.setPosition(6 * Game.tileSize, 7 * Game.tileSize);
+        collisionChecker.checkPlayerTrapCollision(player, trap3);
+
+        player.setPosition(2 * Game.tileSize, 5 * Game.tileSize);
+        collisionChecker.checkPlayerTrapCollision(player, trap4);
+
+        player.setPosition(6 * Game.tileSize, 3 * Game.tileSize);
+        collisionChecker.checkPlayerTrapCollision(player, trap5);
+
+        player.setPosition(4 * Game.tileSize, 3 * Game.tileSize);
+        collisionChecker.checkPlayerTrapCollision(player, trap6);
+
+        double actualScore = player.getScoreObj().getScore();
+        double expectedScore = originalScore - (trap1.getDamage() * 6);
+
+        assertEquals(expectedScore, actualScore);
+    }
+
 }
