fa0c27b84636450eb3e08a5fff2b9df794342070
cas32 <claire_shou@sfu.ca>
2024-04-06 14:53:17 -0700

    new test files for Door entity

---
+++ Code/project/src/test/java/DoorTest.java
@@ -0,0 +1,122 @@
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import Display.Game;
+import Display.Score;
+import Gamestates.Gamestate;
+import Gamestates.Playing;
+import Helpers.CollisionChecker;
+import Helpers.Position;
+import MoveableEntity.Player;
+import StaticEntity.Door;
+import StaticEntity.Reward;
+import StaticEntity.TileManager;
+
+public class DoorTest {
+    private Player player;
+    private static Game game;
+    private static Score scoreObject;
+    private static Playing playing;
+    private static TileManager tileManager;
+    private static CollisionChecker collisionChecker;
+
+    @BeforeAll
+    static void setUpAll() {
+        game = new Game();
+        playing = new Playing(game);
+        tileManager = new TileManager(playing);
+        collisionChecker = new CollisionChecker(tileManager);
+        Gamestate.state = Gamestate.PLAYING;
+    }
+
+    // test for 0 grad caps collected
+    @Test
+    public void testDoorNoGrapCaps() {
+        Door door = new Door(new Position(23 * Game.tileSize, 22 * Game.tileSize));
+        player = new Player(new Position(23 * Game.tileSize, 22 * Game.tileSize), collisionChecker, playing,
+                scoreObject);
+
+        Boolean collideDoor = collisionChecker.checkPlayerDoorCollision(player, door);
+
+        assertTrue(collideDoor);
+        playing.update();
+        assertTrue(!door.getOpen());
+
+    }
+
+    // test for 1 grad cap collected
+    @Test
+    public void testDoorOneGrapCap() {
+        Door door = new Door(new Position(23 * Game.tileSize, 22 * Game.tileSize));
+        Reward reward = new Reward(new Position(5 * Game.tileSize, 21 * Game.tileSize));
+        player = new Player(new Position(5 * Game.tileSize, 21 * Game.tileSize), collisionChecker, playing,
+                scoreObject);
+
+        boolean collideReward = collisionChecker.checkPlayerRewardCollision(player, reward);
+        assertTrue(collideReward);
+
+        player.setPosition(23 * Game.tileSize, 22 * Game.tileSize);
+
+        Boolean collideDoor = collisionChecker.checkPlayerDoorCollision(player, door);
+
+        assertTrue(collideDoor);
+        playing.update();
+        assertTrue(!door.getOpen());
+
+    }
+
+    // test for 2 grad caps collected
+    @Test
+    public void testDoorTwoGrapCaps() {
+        Door door = new Door(new Position(23 * Game.tileSize, 22 * Game.tileSize));
+        Reward reward1 = new Reward(new Position(5 * Game.tileSize, 21 * Game.tileSize));
+        Reward reward2 = new Reward(new Position(20 * Game.tileSize, 11 * Game.tileSize));
+        player = new Player(new Position(5 * Game.tileSize, 21 * Game.tileSize), collisionChecker, playing,
+                scoreObject);
+
+        boolean collideReward1 = collisionChecker.checkPlayerRewardCollision(player, reward1);
+        assertTrue(collideReward1);
+
+        player.setPosition(20 * Game.tileSize, 11 * Game.tileSize);
+        boolean collideReward2 = collisionChecker.checkPlayerRewardCollision(player, reward2);
+        assertTrue(collideReward2);
+
+        player.setPosition(23 * Game.tileSize, 22 * Game.tileSize);
+        Boolean collideDoor = collisionChecker.checkPlayerDoorCollision(player, door);
+
+        assertTrue(collideDoor);
+        playing.update();
+        assertTrue(!door.getOpen());
+    }
+
+    // test for 3 grad caps collected (door opens)
+    @Test
+    public void testDoorThreeGrapCaps() {
+        Door door = new Door(new Position(23 * Game.tileSize, 22 * Game.tileSize));
+        Reward reward1 = new Reward(new Position(5 * Game.tileSize, 21 * Game.tileSize));
+        Reward reward2 = new Reward(new Position(20 * Game.tileSize, 11 * Game.tileSize));
+        Reward reward3 = new Reward(new Position(21 * Game.tileSize, 4 * Game.tileSize));
+        player = new Player(new Position(5 * Game.tileSize, 21 * Game.tileSize), collisionChecker, playing,
+                scoreObject);
+
+        boolean collideReward1 = collisionChecker.checkPlayerRewardCollision(player, reward1);
+        assertTrue(collideReward1);
+
+        player.setPosition(20 * Game.tileSize, 11 * Game.tileSize);
+        boolean collideReward2 = collisionChecker.checkPlayerRewardCollision(player, reward2);
+        assertTrue(collideReward2);
+
+        player.setPosition(21 * Game.tileSize, 4 * Game.tileSize);
+        boolean collideReward3 = collisionChecker.checkPlayerRewardCollision(player, reward3);
+        assertTrue(collideReward3);
+
+        player.setPosition(23 * Game.tileSize, 22 * Game.tileSize);
+        Boolean collideDoor = collisionChecker.checkPlayerDoorCollision(player, door);
+
+        assertTrue(collideDoor);
+        playing.update();
+        // assertTrue(door.getOpen());
+    }
+}
