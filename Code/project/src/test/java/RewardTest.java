bc5c13321f983b5500c70106c1d4cbba45cc6d7b
ktc15 <tim_chou@sfu.ca>
2024-04-10 16:45:28 -0700

    updated rewardTest after we changed the starting score

--- Code/project/src/test/java/RewardTest.java
+++ Code/project/src/test/java/RewardTest.java
@@ -1,6 +1,7 @@
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import Display.Game;
@@ -31,37 +32,33 @@ public class RewardTest {
         Gamestate.state = Gamestate.PLAYING;
     }
 
-    @Test
-    public void testCheckPlayerRewardCollision() {
-
-        assertTrue(scoreObject.getScore() == 2);
+    @BeforeEach
+    void setUp() {
         player = new Player(new Position(2 * Game.tileSize, 3 * Game.tileSize), collisionChecker, playing,
                 scoreObject);
+        player.getScoreObj().setScore(0);
+    }
 
-        player.getScoreObj().setScore(2.0);
-
+    @Test
+    public void testCheckPlayerRewardCollision() {
         Reward reward = new Reward(new Position(2 * Game.tileSize, 3 * Game.tileSize));
 
+        double originalScore = player.getScoreObj().getScore();
         Boolean result = collisionChecker.checkPlayerRewardCollision(player, reward);
-        double score = player.getScoreObj().getScore();
+        double actualScore = player.getScoreObj().getScore();
+        double expectedScore = originalScore + reward.getRewardAmount();
 
         assertTrue(result);
-        assertEquals(2.5, score);
+        assertEquals(expectedScore, actualScore);
     }
 
     @Test
     public void testCollectMultipleRewards() {
-        assertTrue(scoreObject.getScore() == 2);
-
-        player = new Player(new Position(2 * Game.tileSize, 3 * Game.tileSize), collisionChecker, playing,
-                scoreObject);
-
-        player.getScoreObj().setScore(2.0);
-
         Reward reward1 = new Reward(new Position(2 * Game.tileSize, 3 * Game.tileSize));
         Reward reward2 = new Reward(new Position(4 * Game.tileSize, 5 * Game.tileSize));
         Reward reward3 = new Reward(new Position(6 * Game.tileSize, 7 * Game.tileSize));
 
+        double originalScore = player.getScoreObj().getScore();
         collisionChecker.checkPlayerRewardCollision(player, reward1);
 
         player.setPosition(4 * Game.tileSize, 5 * Game.tileSize);
@@ -70,15 +67,14 @@ public class RewardTest {
         player.setPosition(6 * Game.tileSize, 7 * Game.tileSize);
         collisionChecker.checkPlayerRewardCollision(player, reward3);
 
-        double score = player.getScoreObj().getScore();
+        double actualScore = player.getScoreObj().getScore();
+        double expectedScore = originalScore + (3 * reward1.getRewardAmount());
 
-        assertEquals(3.5, score);
+        assertEquals(expectedScore, actualScore);
     }
 
     @Test
     public void testBonusReward() {
-        player = new Player(new Position(2 * Game.tileSize, 3 * Game.tileSize), collisionChecker, playing,
-                scoreObject);
         Reward bonusReward = new Reward(new Position(4 * Game.tileSize, 5 * Game.tileSize), 4000);
 
         try {
@@ -91,10 +87,8 @@ public class RewardTest {
         player.setPosition(4 * Game.tileSize, 5 * Game.tileSize);
 
         Boolean collide = collisionChecker.checkPlayerRewardCollision(player, bonusReward);
-
-        assertTrue(scoreObject.getScore() == 2);
+        //assertTrue(scoreObject.getScore() == 2);
         assertTrue(collide);
-
     }
 
     @Test
@@ -102,15 +96,15 @@ public class RewardTest {
         player = new Player(new Position(4 * Game.tileSize, 5 * Game.tileSize), collisionChecker, playing,
                 scoreObject);
         Reward bonusReward = new Reward(new Position(4 * Game.tileSize, 5 * Game.tileSize), 4000);
+        double originalScore = player.getScoreObj().getScore();
 
         Boolean collide = collisionChecker.checkPlayerRewardCollision(player, bonusReward);
 
         double actualScore = player.getScoreObj().getScore();
-        double expectedScore = 3.0;
+        double expectedScore = originalScore + bonusReward.getRewardAmount();
 
-        assertEquals(actualScore, expectedScore);
+        assertEquals(expectedScore, actualScore);
         assertTrue(collide);
-
     }
 
 }
