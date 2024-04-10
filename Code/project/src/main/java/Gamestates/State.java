9c8837848f57f0c39e703c5c80ab9f0fd935c645
evan <eevanlaw@gmail.com>
2024-04-09 18:48:29 -0700

    added Javadoc comments to the State class

--- Code/project/src/main/java/Gamestates/State.java
+++ Code/project/src/main/java/Gamestates/State.java
@@ -2,14 +2,28 @@ package Gamestates;
 
 import Display.Game;
 
+/**
+ * The State class represents a game state in the application.
+ * It provides access to the game object and serves as a base class for specific game states.
+ */
 public class State {
 
     protected Game game;
 
+    /**
+     * Constructs a State object with the specified game.
+     *
+     * @param game the game object associated with this state
+     */
     public State(Game game) {
         this.game = game;
     }
 
+    /**
+     * Returns the game object associated with this state.
+     *
+     * @return the game object
+     */
     public Game getGame() {
         return game;
     }
