ab954a21f4c8816266e86a30ce8842e8980045ba
evan <eevanlaw@gmail.com>
2024-04-09 18:54:41 -0700

    added Javadoc comments to the KeyboardInputs class

--- Code/project/src/main/java/Helpers/KeyboardInputs.java
+++ Code/project/src/main/java/Helpers/KeyboardInputs.java
@@ -6,14 +6,25 @@ import java.awt.event.KeyListener;
 import Display.GamePanel;
 import Gamestates.Gamestate;
 
+/**
+ * The KeyboardInputs class implements the KeyListener interface and handles keyboard inputs for the game.
+ */
 public class KeyboardInputs implements KeyListener {
 
     private GamePanel gamePanel;
 
+    /**
+     * Constructs a KeyboardInputs object with the specified GamePanel.
+     * @param gamePanel the GamePanel object to associate with the KeyboardInputs
+     */
     public KeyboardInputs(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
     }
 
+    /**
+     * Invoked when a key is pressed.
+     * @param e the KeyEvent object representing the key press event
+     */
     @Override
     public void keyPressed(KeyEvent e) {
         switch (Gamestate.state) {
@@ -30,9 +41,13 @@ public class KeyboardInputs implements KeyListener {
                 gamePanel.getGame().getGameWin().keyPressed(e);
             default:
                 break;
-            }
+        }
     }
 
+    /**
+     * Invoked when a key is released.
+     * @param e the KeyEvent object representing the key release event
+     */
     @Override
     public void keyReleased(KeyEvent e) {
         switch (Gamestate.state) {
@@ -53,6 +68,10 @@ public class KeyboardInputs implements KeyListener {
         }
     }
 
+    /**
+     * Invoked when a key is typed (pressed and released).
+     * @param e the KeyEvent object representing the key typed event
+     */
     @Override
     public void keyTyped(KeyEvent e) {
     }
