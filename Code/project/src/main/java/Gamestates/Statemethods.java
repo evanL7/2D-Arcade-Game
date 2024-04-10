9ab5d7b7b316674ce3d012917da8b905d8ce7260
evan <eevanlaw@gmail.com>
2024-04-09 18:47:25 -0700

    added Javadoc comments to the Statemethods interface

--- Code/project/src/main/java/Gamestates/Statemethods.java
+++ Code/project/src/main/java/Gamestates/Statemethods.java
@@ -3,13 +3,35 @@ package Gamestates;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 
+/**
+ * The Statemethods interface defines the methods that should be implemented by classes representing game states.
+ * These methods are responsible for updating the state, drawing it on the screen, and handling keyboard input events.
+ */
 public interface Statemethods {
 
-	public void update();
+    /**
+     * Updates the state of the game.
+     */
+    public void update();
 
-	public void draw(Graphics g);
+    /**
+     * Draws the state on the screen.
+     *
+     * @param g the Graphics object used for drawing
+     */
+    public void draw(Graphics g);
 
-	public void keyPressed(KeyEvent e);
+    /**
+     * Handles the key press event.
+     *
+     * @param e the KeyEvent object representing the key press event
+     */
+    public void keyPressed(KeyEvent e);
 
-	public void keyReleased(KeyEvent e);
+    /**
+     * Handles the key release event.
+     *
+     * @param e the KeyEvent object representing the key release event
+     */
+    public void keyReleased(KeyEvent e);
 }
