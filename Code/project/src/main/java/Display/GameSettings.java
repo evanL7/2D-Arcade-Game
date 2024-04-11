3b5c7a5ef99ba417ee0aa94aa81920ce12aa5ec3
evan <eevanlaw@gmail.com>
2024-04-11 08:44:15 -0700

    moved attributes in Game class to the GameSettings class

--- Code/project/src/main/java/Display/GameSettings.java
+++ Code/project/src/main/java/Display/GameSettings.java
@@ -2,4 +2,28 @@ package Display;
 
 public class GameSettings {
 
+    private final int FPS_SET = 120;
+    private final int UPS_SET = 200; // Updates per second to prevent the game from running too fast
+
+    public String mapFilePath = "/maps/map1.txt";
+    public String gameTitle = "Grade Quest";
+
+    // Tile settings
+    private final int originalTileSize = 16;
+    private final int scale = 3;
+    public final int tileSize = originalTileSize * scale;
+
+    // Grid size
+    private final int maxScreenCol = 16;
+    private final int maxScreenRow = 12;
+
+    // Scaled screen size
+    public final int screenWidth = tileSize * maxScreenCol;
+    public final int screenHeight = tileSize * maxScreenRow;
+
+    // World size
+    public final int maxWorldCol = 25;
+    public final int maxWorldRow = 25;
+    public final int worldWidth = tileSize * maxWorldCol;
+    public final int worldHeight = tileSize * maxWorldRow;
 }
