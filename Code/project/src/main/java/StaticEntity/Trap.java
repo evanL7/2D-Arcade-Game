01d4cfad51d794a93074fd9f71b2a2ddd0dd8a64
cas32 <claire_shou@sfu.ca>
2024-04-11 16:09:31 -0700

    updated Trap with the new Animation class and removed unused methods

--- Code/project/src/main/java/StaticEntity/Trap.java
+++ Code/project/src/main/java/StaticEntity/Trap.java
@@ -2,11 +2,7 @@ package StaticEntity;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.imageio.ImageIO;
-
+import Animation.Animations;
 import Helpers.Position;
 
 // trap sprite from https://bdragon1727.itch.io/free-trap-platformer
@@ -25,11 +21,9 @@ public class Trap extends StaticEntity {
     /** The amount of damage the trap inflicts. */
     private double damage;
 
-    private BufferedImage[][] animations; // 2d image array of the images for trap movements
-    private int animationTick, animationIndex, animationSpeed = 150;
+    private BufferedImage[][] trapAnimations; // 2d image array of the images for trap movements
+    private Animations animation;
 
-    private BufferedImage trapImage;
-    
     /**
      * Constructs a new Trap.
      *
@@ -40,8 +34,17 @@ public class Trap extends StaticEntity {
         super(position);
         damage = 1; // default value of 1
 
-        loadAnimations();
-        loadTrapImage();
+        animation = new Animations("/assets/trap.png");
+        animation.setAnimationAmount(2);
+        animation.setAnimationArrayHeight(1);
+        animation.setAnimationArrayWidth(2);
+        animation.setSpriteHeight(22);
+        animation.setSpriteWidth(32);
+        animation.setAnimationSpeed(150);
+
+        animation.loadAnimations();
+        animation.loadImage();
+        trapAnimations = animation.getBufferedImages();
     }
 
     // Methods
@@ -58,66 +61,20 @@ public class Trap extends StaticEntity {
     @Override
     public Image getSprite() {
         // Return the sprite image associated with the trap entity
-        return trapImage; // Assuming you have a field named trapImage that holds the sprite image
+        return animation.getImage(); // Assuming you have a field named trapImage that holds the sprite image
     }
-    
 
     // ANIMATION METHODS FOR TRAP
     public void render(Graphics g) {
         Graphics2D g2 = (Graphics2D) g;
-        g2.drawImage(animations[animationIndex][0], position.getX(), position.getY(), gameSettings.getTileSize() + 2, 30,
-            null);
+        g2.drawImage(trapAnimations[animation.getAnimationIndex()][0], position.getX(), position.getY(),
+                gameSettings.getTileSize() + 2,
+                30,
+                null);
     }
 
     public void update() {
-        updateAnimationTick();
+        animation.updateAnimationTick();
     }
 
-    // creates the Image array for the movement animations
-    private void loadAnimations() {
-        InputStream is = getClass().getResourceAsStream("/assets/trap.png");
-
-        try {
-            BufferedImage img = ImageIO.read(is);
-
-            animations = new BufferedImage[2][1];
-            for (int j = 0; j < animations.length; j++) {
-                for (int i = 0; i < animations[j].length; i++) {
-                    animations[j][i] = img.getSubimage(j * 32, i * 22, 32, 22);
-                }
-            }
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            try {
-                is.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    // Load the trap image
-    private void loadTrapImage() {
-        try {
-            InputStream is = getClass().getResourceAsStream("/assets/trap.png");
-            trapImage = ImageIO.read(is);
-            is.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    // updates the animation array during the game loop thread
-    private void updateAnimationTick() {
-        animationTick++;
-        if (animationTick >= animationSpeed) {
-            animationTick = 0;
-            animationIndex++;
-            if (animationIndex >= 2) {
-                animationIndex = 0;
-            }
-        }
-    }
 }
